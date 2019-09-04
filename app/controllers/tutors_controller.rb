3d77321445cf9ba0a2c034735e5c516c41ceb171
jason <jasonedlewis@gmail.com>
2019-09-04 16:35:54 -0400

    added the tutor, student, and sesssions resources to the API

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -1,2 +1,6 @@
 class TutorsController < ApplicationController
+    def index
+        tutors = Tutor.all
+        render json: {message: "This is where the tutors will be, via JS though 🤓"}
+    end
 end
